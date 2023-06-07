bd5fd6ffc9490b47b673bc0afa381b03d226c725
候昱翔 <as0963921757@gmail.com>
2023-06-07 20:04:37 +0800

    8:04

---
+++ LastTest410918657/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastTest410918657.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Music(string name,float? number)
+        {
+            var result = "";
+            if (number.HasValue)
+            {
+                if(number==1)
+                {
+                    result = "do";
+                }
+                else if (number == 2)
+                {
+                    result = "re";
+                }
+                else if (number == 3)
+                {
+                    result = "mi";
+                }
+                else if (number == 4)
+                {
+                    result = "fa";
+                }
+                else if (number == 5)
+                {
+                    result = "so";
+                }
+                else if (number == 6)
+                {
+                    result = "la";
+                }
+                else if (number == 7)
+                {
+                    result = "xi";
+                }
+                else result = "錯誤";
+                    
+            }
+            ViewBag.name = name;
+            ViewBag.number = result;
+            return View();
+        }
+    }
+}
---
+++ LastTest410918657/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LastTest410918657.MvcApplication" Language="C#" %>
